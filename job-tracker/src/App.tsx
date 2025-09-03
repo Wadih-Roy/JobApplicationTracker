18222ab7001cf55740ffa055322640198dbfe80a
Wadih-Roy <wadihroy01@gmail.com>
2025-09-02 22:29:46 -0400

    updated feature to store jobs in localSessions

--- job-tracker/src/App.tsx
+++ job-tracker/src/App.tsx
@@ -1,28 +1,57 @@
-import './App.css'
-import { useState, useRef } from 'react'
+import "./App.css";
+import { useEffect, useState, useRef, use } from "react";
 
-type JobStatus = 'Applied' | 'Interviewing' | 'Offered' | 'Rejected'
-const STATUS: JobStatus[] = ['Applied', 'Interviewing', 'Offered', 'Rejected']
+type JobStatus = "Applied" | "Interviewing" | "Offered" | "Rejected";
+const STATUS: JobStatus[] = ["Applied", "Interviewing", "Offered", "Rejected"];
 
 interface Job {
-  id: string
-  title: string
-  company: string
-  status: JobStatus
-  link?: string
+  id: string;
+  title: string;
+  company: string;
+  status: JobStatus;
+  link?: string;
+}
+
+const LS_KEY = "jobsTracker";
+
+function loadJobs(): Job[] {
+  const jobsJSON = localStorage.getItem(LS_KEY);
+  if (!jobsJSON) return [];
+  try {
+    const jobs = JSON.parse(jobsJSON) as Job[];
+    return jobs;
+  } catch (e) {
+    console.error("Error parsing jobs from localStorage", e);
+    return [];
+  }
+}
+
+function saveJobs(jobs: Job[]) {
+  try{
+    localStorage.setItem(LS_KEY, JSON.stringify(jobs));
+  }catch(e){
+    console.error("Error saving jobs to localStorage", e);
+  }
 }
 
 export function App() {
-  const [title, setTitle] = useState('')
-  const [company, setCompany] = useState('')
-  const [status, setStatus] = useState<JobStatus>('Applied')
-  const [link, setLink] = useState('') 
-  const [jobs, setJobs] = useState<Job[]>([])  
-  const titleRef = useRef<HTMLInputElement>(null)
+  const [title, setTitle] = useState("");
+  const [company, setCompany] = useState("");
+  const [status, setStatus] = useState<JobStatus>("Applied");
+  const [link, setLink] = useState("");
+  const [jobs, setJobs] = useState<Job[]>([]);
+  const titleRef = useRef<HTMLInputElement>(null);
 
+  useEffect(() => {
+    setJobs(loadJobs());   
+  }, []);
+
+  useEffect(() => {
+    saveJobs(jobs)
+  }, [jobs.length]);
   function handleSubmit(e: React.FormEvent) {
-    e.preventDefault()
-    if(!title.trim() || !company.trim() || !status.trim() || (!link)) return
+    e.preventDefault();
+    if (!title.trim() || !company.trim() || !status.trim() || !link) return;
 
     const newJob: Job = {
       id: crypto.randomUUID(),
@@ -30,16 +59,20 @@ export function App() {
       company: company.trim(),
       status: status.trim() as JobStatus,
       link: link.trim() || undefined,
-    }
-    setJobs((prevJobs) => [newJob, ...prevJobs])
+    };
+    setJobs((prevJobs) => [newJob, ...prevJobs]);
 
-    console.log("New Job: ", newJob)
-    setTitle('')
-    setCompany('')
-    setStatus('Applied')
-    setLink('')
+    console.log("New Job: ", newJob);
+    setTitle("");
+    setCompany("");
+    setStatus("Applied");
+    setLink("");
 
-    titleRef.current?.focus()
+    titleRef.current?.focus();
+  }
+
+  function handleDelete(id: string) {
+    return setJobs((prevJobs) => prevJobs.filter((job) => job.id !== id));
   }
 
   return (
@@ -49,79 +82,83 @@ export function App() {
       {/* FORM SECTION */}
       <form onSubmit={handleSubmit} className="mb-6">
         <div className="mb-4">
-          <label className='block text-sm font medium mb-1'>
-            Job Title
-          </label>
+          <label className="block text-sm font medium mb-1">Job Title</label>
 
-          <input 
-          id='title'
-          ref={titleRef}
-          value={title} 
-          onChange={ (e) => setTitle(e.target.value)}
-          className='w-full border border rounded px-3 py-2'
-          placeholder='Software Developer'
+          <input
+            id="title"
+            ref={titleRef}
+            value={title}
+            onChange={(e) => setTitle(e.target.value)}
+            className="w-full border border rounded px-3 py-2"
+            placeholder="Software Developer"
           />
         </div>
 
         <div className="mb-4">
-          <label className='block text-sm font-medium mb-1'> Company </label>
+          <label className="block text-sm font-medium mb-1"> Company </label>
           <input
-          id='company'
+            id="company"
             value={company}
             onChange={(e) => setCompany(e.target.value)}
-            className='w-full border border rounded px-3 py-2'
-            placeholder='Google'
+            className="w-full border border rounded px-3 py-2"
+            placeholder="Google"
           />
         </div>
 
         <div className="mb-4">
-          <label className='block text-sm font medium mb-1'> Status </label>
-          <select 
-          id='status'
-          value={status}
-          onChange={(e) => setStatus(e.target.value as JobStatus)}
-          className='w-full border border rounded px-3 py-2'>
+          <label className="block text-sm font medium mb-1"> Status </label>
+          <select
+            id="status"
+            value={status}
+            onChange={(e) => setStatus(e.target.value as JobStatus)}
+            className="w-full border border rounded px-3 py-2"
+          >
             {STATUS.map((statusOption) => (
               <option key={statusOption} value={statusOption}>
                 {statusOption}
               </option>
             ))}
           </select>
-
         </div>
 
         <div className="mb-4">
-          <label className='block text-sm font medium mb-1'>Application Link</label>
-          <input 
-          id='link'
+          <label className="block text-sm font medium mb-1">
+            Application Link
+          </label>
+          <input
+            id="link"
             value={link}
-            type='url'
-            inputMode='url'
+            type="url"
+            inputMode="url"
             onChange={(e) => setLink(e.target.value)}
-            className='w-full border border rounded px-3 py-2'
-            placeholder='Site URL'
+            className="w-full border border rounded px-3 py-2"
+            placeholder="Site URL"
           />
         </div>
 
-        <button 
-        type='submit'
-        className='bg-blue-600 text-white px-4 py-2 rounded hover: bg-blue-700' >
+        <button
+          type="submit"
+          className="bg-blue-600 text-white px-4 py-2 rounded hover: bg-blue-700"
+        >
           Add Job
         </button>
       </form>
 
       {/* JOB LIST SECTION */}
 
-      <section className='max-w-xl'>
-        <h2 className='txt-xl font-semibold mb-4'> Job List </h2>
+      <section className="max-w-xl">
+        <h2 className="txt-xl font-semibold mb-4"> Job List </h2>
         {jobs.length === 0 ? (
-          <p className='text-gray-600'> No jobs added yet. </p>
+          <p className="text-gray-600"> No jobs added yet. </p>
         ) : (
-          <ul className='space-y-3'>
+          <ul className="space-y-3">
             {jobs.map((job) => (
-              <li key={job.id} className='text-gray-600'>
+              <li key={job.id} className="text-gray-600">
                 <div className="font-medium">
-                  {job.title} at {job.company && <span className="text-gray=600">@ {job.company} </span>}
+                  {job.title} at{" "}
+                  {job.company && (
+                    <span className="text-gray=600">@ {job.company} </span>
+                  )}
                 </div>
                 <div className="text-sm text-gray-500">{job.status}</div>
                 {job.link && (
@@ -134,11 +171,18 @@ export function App() {
                     {job.link}
                   </a>
                 )}
+                {/* DELETE BUTTON */}
+                <button
+                  onClick={() => handleDelete(job.id)}
+                  className="ml-4 text-red-600 hover:underline text-sm"
+                >
+                  Delete
+                </button>
               </li>
             ))}
           </ul>
         )}
       </section>
     </div>
-  )
+  );
 }
